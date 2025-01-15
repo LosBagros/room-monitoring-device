38e003797e939412047a9035f08ac4b452207099
Tomas Kroupa <tom@bagros.eu>
2025-01-15 19:55:34 +0100

    proof of concept blikani

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <time.h>
+#include <TaskScheduler.h>
 
 #include <secret.h>
 
@@ -17,6 +18,13 @@
 #define NUMPIXELS 2
 #define SDA 5
 #define SCL 6
+
+// Callback methods prototypes
+void ledsCallback();
+
+Task t1(1000, TASK_FOREVER, &ledsCallback);
+
+Scheduler runner;
 
 char mqttTopic[50];
 
@@ -28,7 +36,7 @@ Adafruit_SHT4x sht4 = Adafruit_SHT4x();
 SensirionI2CScd4x scd4x;
 // TODO: Light sensor is not visible in I2C
 
-void SetupTime()
+void setupTime()
 {
   configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org");
   setenv("TZ", Timezone, 1);
@@ -66,21 +74,51 @@ void setColor(uint8_t r, uint8_t g, uint8_t b)
   strip.show();
 }
 
-void fadeSingleColor(uint8_t r, uint8_t g, uint8_t b, uint8_t maxBrightness)
+void ledsCallback()
+{
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    if (co2 > 2000)
+      fadeSingleColor(255, 0, 0, 255, 1000);
+    else if (co2 > 1000)
+      fadeSingleColor(255, 100, 0, 255, 1000);
+    else if (co2 < 1000 && co2 > 1)
+      fadeSingleColor(0, 255, 0, 255, 1000);
+  }
+  else if (!mqttClient.connected())
+  {
+    if (co2 > 2000)
+      fadeSingleColor(255, 0, 0, 255, 2000);
+    else if (co2 > 1000)
+      fadeSingleColor(255, 100, 0, 255, 2000);
+    else if (co2 < 1000 && co2 > 1)
+      fadeSingleColor(0, 255, 0, 255, 2000);
+  }
+  else
+  {
+    if (co2 > 2000)
+      setColor(255, 0, 0);
+    else if (co2 > 1000)
+      setColor(255, 100, 0);
+    else if (co2 < 1000 && co2 > 1)
+      setColor(0, 255, 0);
+  }
+}
+
+void fadeSingleColor(int r, uint8_t g, int b, int maxBrightness, int lightDuration)
 {
   if (maxBrightness < 0)
-  {
     return;
-  }
   if (maxBrightness > 255)
-  {
     maxBrightness = 255;
-  }
+
   for (int brightness = 0; brightness <= maxBrightness; brightness += 10)
   {
     setColor((r * brightness) / 255, (g * brightness) / 255, (b * brightness) / 255);
     delay(100);
   }
+
+  delay(lightDuration);
 
   for (int brightness = maxBrightness; brightness >= 0; brightness -= 10)
   {
@@ -89,76 +127,48 @@ void fadeSingleColor(uint8_t r, uint8_t g, uint8_t b, uint8_t maxBrightness)
   }
 }
 
-void connectToWiFi()
+void WiFiStationDisconnected(WiFiEvent_t event, WiFiEventInfo_t info)
 {
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    return;
-  }
-  WiFi.mode(WIFI_STA);
-  WiFi.setTxPower(WIFI_POWER_5dBm);
+  Serial.println("Disconnected from WiFi access point");
+  Serial.print("WiFi lost connection. Reason: ");
+  Serial.println(info.wifi_sta_disconnected.reason);
+  Serial.println("Trying to Reconnect");
   WiFi.begin(ssid, password);
-  Serial.print("Connecting to WiFi ..");
-  int counter = 0;
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    fadeSingleColor(0, 0, 255, 100);
-    delay(500);
-    Serial.print(".");
-    counter++;
-    if (counter > 30)
-    {
-      Serial.println("Failed to connect to WiFi");
-      ESP.restart();
-    }
-  }
-  Serial.println("\nConnected to the WiFi network");
-  Serial.print("IP Address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("RSSI: ");
-  Serial.print(WiFi.RSSI());
-  Serial.println(" dBm");
 }
 
 int mqttTries = 0;
 void connectToMQTTBroker()
 {
-  connectToWiFi();
-  mqttTries = 0;
-  while (!mqttClient.connected())
+  if (WiFi.status() != WL_CONNECTED)
   {
-    Serial.print("RSSI: ");
-    Serial.print(WiFi.RSSI());
-    Serial.println(" dBm");
-    Serial.print("Connecting to MQTT...");
-    mqttClient.setServer(mqttServer, mqttPort);
-    if (mqttClient.connect(sensorId, mqttUser, mqttPassword))
-    {
-      Serial.println("connected");
-    }
-    else
-    {
-      Serial.print(" failed with state ");
-      Serial.println(mqttClient.state());
-      delay(2000);
-    }
-    mqttTries++;
-    if (mqttTries > 30)
-    {
-      ESP.restart();
-    }
+    Serial.println("WiFi not connected");
+    return;
   }
+  Serial.print("Connecting to MQTT...");
+  mqttClient.setServer(mqttServer, mqttPort);
+  if (mqttClient.connect(sensorId, mqttUser, mqttPassword))
+  {
+    Serial.println("connected");
+    mqttTries = 0;
+  }
+  else
+    Serial.println(" failed with state " + mqttClient.state());
 }
 
 void setup()
 {
   Serial.begin(115200);
   strip.begin();
-
-  connectToWiFi();
-  SetupTime();
-
+  setColor(255, 255, 255);
+  setupTime();
+  runner.init();
+  t1.enable();
   Wire.begin(SDA, SCL);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.setTxPower(WIFI_POWER_5dBm);
+  WiFi.begin(ssid, password);
+  WiFi.onEvent(WiFiStationDisconnected, WiFiEvent_t::ARDUINO_EVENT_WIFI_STA_DISCONNECTED);
 
   if (!sht4.begin())
   {
@@ -193,27 +203,32 @@ void setup()
 }
 
 sensors_event_t hum, temp;
-uint32_t timestamp = 0;
 uint16_t co2 = 0;
 float co2Temperature = 0.0f;
 float co2Humidity = 0.0f;
 uint16_t error = 0;
 char errorMessage[256];
 bool isDataReady = false;
+unsigned long lastMillis = 0;
 
 StaticJsonDocument<256> jsonDoc;
 
 void loop()
 {
+  runner.execute();
   hum.relative_humidity = 0.0f;
   temp.temperature = 0.0f;
-  timestamp = 0;
   co2 = 0;
   co2Temperature = 0.0f;
   co2Humidity = 0.0f;
   error = 0;
   isDataReady = false;
 
+  if (millis() - lastMillis < 10000)
+  {
+    return;
+  }
+  lastMillis = millis();
   error = scd4x.getDataReadyFlag(isDataReady);
   if (error)
   {
@@ -221,14 +236,11 @@ void loop()
     errorToString(error, errorMessage, 256);
     Serial.println(errorMessage);
     Serial.println();
-    delay(1000);
     return;
   }
   if (!isDataReady)
   {
     Serial.println("Data not ready yet");
-    fadeSingleColor(255, 100, 0, 100);
-    delay(1000);
     return;
   }
 
@@ -253,9 +265,7 @@ void loop()
   }
   Serial.println();
 
-  timestamp = millis();
   sht4.getEvent(&hum, &temp);
-  timestamp = millis() - timestamp;
 
   Serial.print("Temperature: ");
   Serial.print(temp.temperature);
@@ -263,23 +273,8 @@ void loop()
   Serial.print("Humidity: ");
   Serial.print(hum.relative_humidity);
   Serial.print("% ");
-  Serial.print(" Read duration (ms): ");
-  Serial.print(timestamp);
-  Serial.println();
   Serial.println();
 
-  if (co2 > 2000)
-  {
-    setColor(255, 0, 0);
-  }
-  else if (co2 > 1000)
-  {
-    setColor(255, 100, 0);
-  }
-  else if (co2 < 1000 && co2 > 1)
-  {
-    setColor(0, 255, 0);
-  }
   if (co2 <= 0)
   {
     Serial.println("CO2 Reading failed");
@@ -303,5 +298,4 @@ void loop()
 
   mqttClient.publish(mqttTopic, jsonBuffer);
   mqttClient.loop();
-  delay(10000);
 }
